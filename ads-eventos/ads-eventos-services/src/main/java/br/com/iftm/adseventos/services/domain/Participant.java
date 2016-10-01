bd48f7b804e526f435100cecbd992563d8945ae8
Philipe Alves de Oliveira e Silva <philipealvess@gmail.com>
2016-09-30 22:01:37 -0300

    Add domain entities

---
+++ ads-eventos/ads-eventos-services/src/main/java/br/com/iftm/adseventos/services/domain/Participant.java
@@ -0,0 +1,37 @@
+package br.com.iftm.adseventos.services.domain;
+
+public class Participant {
+
+	private Long id;
+	private String name;
+	private String email;
+
+	public Participant() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
